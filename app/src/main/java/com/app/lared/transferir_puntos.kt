f2d851c1bea504b18c2a7293355d37b28b8d95e0
Luis Gabriel Malagon <malagondiaz@gmail.com>
2019-06-27 18:45:14 -0500

    Versión inicial sin programación

---
+++ app/src/main/java/com/app/lared/transferir_puntos.kt
@@ -0,0 +1,12 @@
+package com.app.lared
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class transferir_puntos : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transferir_puntos)
+    }
+}
--- app/src/main/java/com/app/lared/uso_app_cliente.kt
+++ app/src/main/java/com/app/lared/uso_app_cliente.kt
@@ -11,10 +11,29 @@ class uso_app_cliente : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_uso_app_cliente)
 
+        val terminarSesion : Button = findViewById(R.id.salir_delared)
+        terminarSesion.setOnClickListener {
+            val intent = Intent(this, MainActivity :: class.java)
+            startActivity(intent)
+        }
+
         val buscarNegociosAliados : Button = findViewById(R.id.conocer_aliados)
         buscarNegociosAliados.setOnClickListener {
             val intent = Intent(this, buscar_aliados :: class.java)
             startActivity(intent)
         }
+
+        val transferirPuntos : Button = findViewById(R.id.transferir_puntos)
+        transferirPuntos.setOnClickListener {
+            val intent = Intent(this, transferir_puntos :: class.java)
+            startActivity(intent)
+        }
+
+        val buscarNegociosPromocion : Button = findViewById(R.id.negocios_promocion)
+        buscarNegociosPromocion.setOnClickListener {
+            val intent = Intent(this, buscar_promocion :: class.java)
+            startActivity(intent)
+        }
+
     }
 }
