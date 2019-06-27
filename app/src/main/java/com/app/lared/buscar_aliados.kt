ea99b9a224a6e82ac32f8d08e5bf9b569e4ee278
Luis Gabriel Malagon <malagondiaz@gmail.com>
2019-06-27 17:12:35 -0500

    Versión inicial sin programación

---
+++ app/src/main/java/com/app/lared/buscar_aliados.kt
@@ -0,0 +1,12 @@
+package com.app.lared
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class buscar_aliados : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buscar_aliados)
+    }
+}
--- app/src/main/java/com/app/lared/uso_app_cliente.kt
+++ app/src/main/java/com/app/lared/uso_app_cliente.kt
@@ -1,12 +1,20 @@
 package com.app.lared
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class uso_app_cliente : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_uso_app_cliente)
+
+        val buscarNegociosAliados : Button = findViewById(R.id.conocer_aliados)
+        buscarNegociosAliados.setOnClickListener {
+            val intent = Intent(this, buscar_aliados :: class.java)
+            startActivity(intent)
+        }
     }
 }
